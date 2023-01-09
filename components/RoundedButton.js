29af66e91f4fae7cf42fd214018795756f8f5bcc
Andre Fong <fongandre123@gmail.com>
2023-01-08 22:24:41 -0500

    feat: Finish sign in form

--- components/RoundedButton.js
+++ components/RoundedButton.js
@@ -2,14 +2,19 @@ import React from "react";
 import styles from "../styles/Button.module.scss";
 
 /**
- *
  * @param {String} text Button text
  * @param {Number} padding Button padding
  * @param {String} primary Primary color
  * @param {String} secondary Secondary color
- * @param {String} link Relative link
+ * @param {Boolean} submit Submit button for forms
  */
-export default function RoundedButton({ text, padding, primary, secondary }) {
+export default function RoundedButton({
+  text,
+  padding,
+  primary,
+  secondary,
+  submit,
+}) {
   return (
     <button
       className={styles.RoundedBtn}
@@ -19,6 +24,7 @@ export default function RoundedButton({ text, padding, primary, secondary }) {
         color: secondary,
         border: `1.2px solid ${secondary}`,
       }}
+      type={submit ? "submit" : "button"}
     >
       {text}
     </button>
--- pages/signin/index.js
+++ pages/signin/index.js
@@ -1,9 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/Signin.module.scss";
+import { useRouter } from "next/router";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import LockIcon from "@mui/icons-material/Lock";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import RoundedButton from "../../components/RoundedButton";
 
 export default function Signin() {
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Handles submitting signin form
+  function handleSubmit(e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("/api/sessions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        if (res.status !== 201) throw new Error("Failed to sign in");
+        console.log("Signed in");
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  }
+
   return (
-    <div>
-      <div></div>
+    <div className={styles.content}>
+      <div className={styles.modal}>
+        <Link href="/">
+          <div className={styles.logo}>
+            <Image
+              src="/android-chrome-512x512.png"
+              alt="UniSpaces logo"
+              width={30}
+              height={30}
+            />
+            <h2 className={styles.unispaces}>UniSpaces</h2>
+          </div>
+        </Link>
+        <h1 className={styles.title}>Sign in</h1>
+        <div className={styles.subtitle}>to continue to UniSpaces</div>
+
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          autoComplete="off"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+          width="100%"
+        >
+          <FormControl sx={{ m: 2, width: "300px" }} variant="outlined">
+            <InputLabel htmlFor="username">Username</InputLabel>
+            <OutlinedInput
+              id="username"
+              name="username"
+              type="text"
+              startAdornment={
+                <InputAdornment position="start">
+                  <AccountCircleIcon />
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+
+          <FormControl sx={{ m: 2, width: "300px" }} variant="outlined">
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              name="password"
+              type={showPassword ? "text" : "password"}
+              startAdornment={
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              }
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Toggle password visibility"
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                    }}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <div className={styles.bottom}>
+            <Link href="/signup">
+              <span className={styles.create_account}>Create account</span>
+            </Link>
+            <RoundedButton
+              text="Confirm"
+              primary="rgb(255, 77, 77)"
+              secondary="white"
+              submit
+            />
+          </div>
+        </Box>
+      </div>
     </div>
   );
 }
